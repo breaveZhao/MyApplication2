73ccf51aebd4fd05ac29d429f5fc6f09f58eb67e
ZY <475282717@qq.com>
2017-10-23 17:08:59 +0800

    Initial commit

---
+++ textutlis/src/main/java/com/socool/textutlis/TextUtils.java
@@ -0,0 +1,14 @@
+package com.socool.textutlis;
+
+import android.util.Log;
+
+/**
+ * Created by 李良海 on 2017/10/23.
+ * Function：
+ */
+
+public class TextUtils {
+    public static void Sys(String res){
+        Log.e("TAG_UTILS",res);
+    }
+}
